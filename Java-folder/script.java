32fab525d05da8165963b36e836c7594046176de
Muhammad Bilal <154779060+Bilal3850@users.noreply.github.com>
2024-09-23 08:56:39 +0000

    commet

--- Java-folder/script.java
+++ Java-folder/script.java
@@ -3,7 +3,12 @@ public class script {
     public static void main(String[] args) {
         System.out.println("my name is Bilal.");
         for (int i = 0; i < 11; i++) {
-            System.out.println("bilal " + i);
+            System.out.print("bilal " + i + " ");
+        }
+        int i = 1;
+        while (i<=10) {
+            System.out.print(i + " ");
+            i++;
         }
         
     }
