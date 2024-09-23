442020bc343e2fc77223fa12f0adbb3a8b9085df
Muhammad Bilal <154779060+Bilal3850@users.noreply.github.com>
2024-09-23 05:08:41 +0000

    comment

---
+++ Java-folder/script.java
@@ -0,0 +1,10 @@
+public class script {
+
+    public static void main(String[] args) {
+        System.out.println("my name is Bilal.");
+        for (int i = 0; i < 11; i++) {
+            System.out.println("bilal " + i);
+        }
+        
+    }
+}
