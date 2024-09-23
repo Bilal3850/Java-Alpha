f69fd931c5574cf6e3f76fb2ef8664c395cf6310
Muhammad Bilal <154779060+Bilal3850@users.noreply.github.com>
2024-09-23 18:29:38 +0000

    updates

---
+++ Variables and Data Structure/OutPut.java
@@ -0,0 +1,13 @@
+public class OutPut {
+
+    public static void main(String[] args) {
+        System.out.print("My name is \n Bilal.\n");
+        
+        // \n is for next line
+
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
---
+++ Variables and Data Structure/input.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class input {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // String name = sc.nextLine();
+        // // boolean bool = sc.nextBoolean();
+        // System.out.println(name);
+
+        /*
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        int product = a * b;
+        System.out.println(sum);
+        System.out.println(product);
+        */
+
+        int r = sc.nextInt();
+        float area = 3.14f * r * r;
+        System.out.println(area);
+
+    }
+}
