833b61e5e945e2c9aeebd1b1b074805bc651de8a
Muhammad Bilal <154779060+Bilal3850@users.noreply.github.com>
2024-09-24 16:28:00 +0000

    sdf

--- Conditional Statements/ifelse.java
+++ Conditional Statements/ifelse.java
@@ -43,5 +43,25 @@ public class ifelse {
         // int num = sc.nextInt();
         // String paper = (num >= 33) ? "Pass" : "Fail";
         // System.out.println(paper);
+
+        int num = sc.nextInt();
+        switch (num) {
+            case 1: System.out.println("Monday");
+                
+            case 2: System.out.println("Tuesday");
+                
+            case 3: System.out.println("Wednesday");
+                break;
+            case 4: System.out.println("Thursday");
+                break;
+            case 5: System.out.println("Friday");
+                break;
+            case 6: System.out.println("Saturday");
+                break;
+            case 7: System.out.println("Sunday");
+                break;
+            default: System.out.println("There is nothing");
+                break;
+        }
     }
 }
