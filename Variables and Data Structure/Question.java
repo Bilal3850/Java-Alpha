91beac894c78a70dacd369ae3371ee6f1afc366b
Muhammad Bilal <154779060+Bilal3850@users.noreply.github.com>
2024-09-24 08:19:21 +0000

    commit

---
+++ Variables and Data Structure/Question.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Question {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Question    1
+        int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // int sum = a + b + c;
+        // float Avg = sum / 3;
+        // System.out.println("The Average of Three No is " + Avg);
+
+        // Question     2
+        float Area = a * a;
+        System.out.println("Area of Square is " + Area);
+
+    }
+}
---
+++ Variables and Data Structure/typecomvertion.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+public class typecomvertion {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Type Convertion
+        // int a = 14;
+        // float b = a;
+        // System.out.println(b);
+
+        // type Casting
+        // float a = 10.312f;
+        // int b = (int) a;
+        // System.out.println(b);
+
+        char ch = 'a';
+        char ch2 = 'b';
+        int num = ch;
+        int num2 = ch2;
+        System.out.println(num);
+        System.out.println(num2);
+        
+    }
+}
